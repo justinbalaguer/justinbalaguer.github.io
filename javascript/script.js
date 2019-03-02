ab672a91f06db47a713b34f5517792713a2b7f46
Justin Anthony Balaguer I <justinanthony.primary@gmail.com>
2019-03-03 00:48:23 +0800

    website update

--- javascript/script.js
+++
@@ -1,7 +0,0 @@
-var animation = bodymovin.loadAnimation({
-    container: document.getElementById('charAnimation'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'data.json'
-});
