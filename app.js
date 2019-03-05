ca51fbc9fa447ecd6fe304baac248a7882a8b72d
Justin Anthony Balaguer I <justinanthony.primary@gmail.com>
2019-03-05 21:16:38 +0800

    fixed project links (view project)

--- app.js
+++ app.js
@@ -32,22 +32,22 @@ class App extends React.Component {
     Name7: data[7].name,
     Description7: data[7].description,
     Language7: data[7].language,
-    Html_Url7: data[7].owner.html_url,
+    Html_Url7: data[7].html_url,
     //Ghost File
     Name1: data[1].name,
     Description1: data[1].description,
     Language1: data[1].language,
-    Html_Url1: data[1].owner.html_url,
+    Html_Url1: data[1].html_url,
     //Unity Game
     Name0: data[0].name,
     Description0: data[0].description,
     Language0: data[0].language,
-    Html_Url0: data[0].owner.html_url,
+    Html_Url0: data[0].html_url,
     //Github Repository Fetch
     Name2: data[2].name,
     Description2: data[2].description,
     Language2: data[2].language,
-    Html_Url2: data[2].owner.html_url
+    Html_Url2: data[2].html_url
   })
 }
   render() {
@@ -65,28 +65,28 @@ class App extends React.Component {
                         <td className='text-center'>{this.state.Name7}</td>
                         <td>{this.state.Description7}</td>
                         <td className='text-center'>{this.state.Language7}</td>
-                        <td className='text-center'><a href={this.state.Html_Url7}>view project</a></td>
+                        <td className='text-center'><a href={this.state.Html_Url7} rel="noopener noreferrer" target="_blank">view project</a></td>
                         <td className='text-center'><a href="https://justinbalaguer.github.io/ph-news" rel="noopener noreferrer" target="_blank">Demo</a></td>
                     </tr>
                     <tr>
                         <td className='text-center'>{this.state.Name1}</td>
                         <td>{this.state.Description1}</td>
                         <td className='text-center'>{this.state.Language1}</td>
-                        <td className='text-center'><a href={this.state.Html_Url1}>view project</a></td>
+                        <td className='text-center'><a href={this.state.Html_Url1} rel="noopener noreferrer" target="_blank">view project</a></td>
                         <td className='text-center'><a href="https://www.dropbox.com/s/ahg2i16i1t46jv0/Ghost%20File%20v1.0%20BETA.exe?dl=0" rel="noopener noreferrer" target="_blank">Demo</a></td>
                     </tr>
                     <tr>
                         <td className='text-center'>{this.state.Name0}</td>
                         <td>{this.state.Description0}</td>
                         <td className='text-center'>{this.state.Language0}</td>
-                        <td className='text-center'><a href={this.state.Html_Url0}>view project</a></td>
+                        <td className='text-center'><a href={this.state.Html_Url0} rel="noopener noreferrer" target="_blank">view project</a></td>
                         <td className='text-center'><a href="https://www.dropbox.com/s/2f2403neqnbjwvy/Evade.apk?dl=0" rel="noopener noreferrer" target="_blank">Demo</a></td>
                     </tr>
                     <tr>
                         <td className='text-center'>{this.state.Name2}</td>
                         <td>{this.state.Description2}</td>
                         <td className='text-center'>{this.state.Language2}</td>
-                        <td className='text-center'><a href={this.state.Html_Url2}>view project</a></td>
+                        <td className='text-center'><a href={this.state.Html_Url2} rel="noopener noreferrer" target="_blank">view project</a></td>
                         <td className='text-center'><a href="https://justinbalaguer.github.io/github-repo-fetch" rel="noopener noreferrer" target="_blank">Demo</a></td>
                     </tr>
                     </tbody>
