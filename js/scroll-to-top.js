07b6e93bc4ba52e1ac66b8591ddda088cc4a27e2
justinbalaguer <justinanthony.primary@gmail.com>
2019-04-04 01:05:27 +0800

    added scroll to top

---
+++ js/scroll-to-top.js
@@ -0,0 +1,10 @@
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
